51b3530aa91e72d22f75839534b7b726c07647a7
trannguyensinh <trannguyensinh2004@gmail.com>
2020-11-12 13:52:32 +0700

    done prefix

--- bot.js
+++ bot.js
@@ -1,1 +1,26 @@
 require('dotenv').config();
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '$';
+
+client.login(process.env.TOKEN);
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    
+    if (msg.content.startsWith(prefix))
+    {
+        const [command, ...args] = msg.content.trim().substring(prefix.length).split(/\s+/);
+
+        console.log();
+
+    }
+
+
+});
